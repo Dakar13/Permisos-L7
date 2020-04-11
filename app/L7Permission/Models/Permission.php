9f48bcfe387b88d6b7ae6660a65cee6cd734917f
Dakar13 <ecalvamtz@gmail.com>
2020-04-10 22:18:46 -0500

    Modelos y Migraciones de Role, Usuarios y Permisos creados y funcionando

---
+++ app/L7Permission/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\L7Permission\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    //
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\L7Permission\Models\Role;
+use App\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function () {
+    
+  /*  return Role::create([
+        'name' => 'Admin',
+        'slug' => 'admin',
+        'description' => 'Administrator',
+        'full-access' => 'yes'
+    ]);
+
+    return Role::create([
+        'name' => 'Guest',
+        'slug' => 'guest',
+        'description' => 'guest',
+        'full-access' => 'no'
+    ]);
+
+    return Role::create([
+        'name' => 'Test',
+        'slug' => 'test',
+        'description' => 'test',
+        'full-access' => 'no'
+    ]);
+    */
+    
+    $user = User::find(1);
+    // $user->roles()->attach([1,4,6]);
+    // $user->roles()->detach([4]);
+    $user->roles()->sync([1]);
+
+    // return $user;
+    return $user->roles;
+
+});
